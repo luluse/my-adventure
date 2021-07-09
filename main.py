21e139840df93a92c06c4d4a0a966e7baaea0329
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-09 10:05:47 -0700

    added if conditions for age requirement

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 print("hello you")
 name = input("What is your name? ")
-age = input("What is your age? ")
+age = int(input("What is your age? "))
 
 print("Hello", name, "your are", age, "years old")
+
+if age >= 18:
+    print("You are old enough to play!")
+
+    wants_to_play = input("Do you want to play? ")
+    if wants_to_play == "yes": 
+      print("let's play")
+      
+elif age >= 14:
+    print("You can play with a supervisor")
+else:
+    print("You are not old enough to play")
