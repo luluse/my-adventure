95aa9f82a27effe7474b1787eb7aa7638d9d3577
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-13 09:14:30 -0700

    added health and more prompts

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ age = int(input("What is your age? "))
 
 print("Hello", name, "your are", age, "years old")
 
+health = 10
+
+print("You are starting with", health, "health")
+
 if age >= 18:
     print("You are old enough to play!")
 
-    wants_to_play = input("Do you want to play? ")
+    wants_to_play = input("Do you want to play? ").lower()
     if wants_to_play == "yes": 
-      print("let's play")
+        print("let's play!")
+
+        left_or_right = input("First Choice... Left or Right (left/right)? ")
+        if left_or_right == "left":
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around? (across/around)? ")
+
+            if ans == "around":
+                print("You went around and reach the other side of the lake")
+
+            elif ans == "across":
+                print("You managed to get across, but were bit by an Eel and lost 5 health.")
+                health -= 5
+
+
+        else:
+            print("You fell into a ravine and lost...")
+
+
+    else:
+        print("See you later...")
       
 elif age >= 14:
     print("You can play with a supervisor")
